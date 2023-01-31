bf3d9d960c3adcf92df31e44d3b10da8281af700
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-01-31 16:26:23 +0700

    Update App.tsx

--- app/App.tsx
+++ app/App.tsx
@@ -1,5 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
@@ -12,9 +12,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    margin: 0,
+    padding: 0,
   },
 });
