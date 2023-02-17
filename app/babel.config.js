2901688021ef7e722ff783967f7b97a7a86f322c
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-02-17 17:20:27 +0700

    config env

--- app/babel.config.js
+++ app/babel.config.js
@@ -3,17 +3,16 @@ module.exports = function (api) {
   return {
     presets: ["babel-preset-expo"],
     plugins: [
-      [
-        "module:react-native-dotenv",
-        {
-          moduleName: "@env",
-          path: ".env",
-          blacklist: null,
-          whitelist: null,
-          safe: false,
-          allowUndefined: true,
-        },
-      ],
+      // [
+      //   "module:react-native-dotenv",
+      //   {
+      //     moduleName: "@env",
+      //     path: ".env",
+      //     safe: true,
+      //     allowUndefined: false,
+      //   },
+      // ],
+      "transform-inline-environment-variables",
     ],
   };
 };
---
+++ app/src/config/config.ts
@@ -0,0 +1,6 @@
+import Constants from "expo-constants";
+// import ENDPOINT_URL from '@env';
+
+import * as dotenv from 'dotenv';
+
+const config = { ENDPOINT_URL } = process.env;
