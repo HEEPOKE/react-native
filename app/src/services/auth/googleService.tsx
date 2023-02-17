837d950d90add36a830da733a11460305ddd95f0
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-02-17 17:03:28 +0700

    set env && remove import firebase

--- app/src/services/auth/googleService.tsx
+++ app/src/services/auth/googleService.tsx
@@ -1,4 +1,3 @@
-import auth from '@react-native-firebase/auth';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 
 GoogleSignin.configure({
@@ -12,8 +11,8 @@ async function onGoogleButtonPress() {
   const { idToken } = await GoogleSignin.signIn();
 
   // Create a Google credential with the token
-  const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+  // const googleCredential = auth.GoogleAuthProvider.credential(idToken);
 
   // Sign-in the user with the credential
-  return auth().signInWithCredential(googleCredential);
+  // return auth().signInWithCredential(googleCredential);
 }
