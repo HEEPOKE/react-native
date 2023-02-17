fa5e0d0490130f59d8307e684fda7cc2724b942d
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-02-17 16:57:20 +0700

    remove firebase

--- app/src/services/auth/authServices.ts
+++
@@ -1,9 +0,0 @@
-import configDB from "../../config/db";
-
-const registerService = (payload: any) => {
-  return configDB.firebaseApp.collection("user").doc().set(payload);
-};
-
-const authServices = { registerService };
-
-export default authServices;
